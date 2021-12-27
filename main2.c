2a707387bb5b8625adaa9a9f16a83022c9d6fa98
yura <yura.nabokov1990@mail.ru>
2021-12-27 21:01:07 +0400

    first commit

---
+++ main2.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include<stdio.h>
+int main(){
+//set birth year
+int birthYear = 2004;
+//set test year
+int testYear = 2010;
+//calculate age
+int age;
+age = testYear - birthYear;
+printf("Age of student %d in year %d\n", age,testYear);
+}
